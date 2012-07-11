f5c9e3b64da32e5ab392f49e4e12d70a49f1c70b
Ric Allinson <capecodehq@yahoo.co.uk>
2012-07-11 09:38:21 -0700

    Created example webapp builds

---
+++ builds/webapp/tablet/yahoo.hybrid.newsfeed/models/newsfeed.common.js
@@ -0,0 +1,255 @@
+/*
+ * Copyright (c) 2012 Yahoo! Inc. All rights reserved.
+ */
+
+/*jslint regexp: true */
+
+/*global YUI: true*/
+
+'use strict';
+
+YUI.add('newsfeedmodel', function (Y, NAME) {
+
+    var cache = {}, // An ordered array of cached objects
+        regexps = {
+            replaceBrsRe:   /(<br[^>]*>[ \n\r\t]*){2,}/gi,
+            replaceFontsRe: /<(\/?)font[^>]*>/gi,
+            trimRe:         /^\s+|\s+$/g,
+            normalizeRe:    /\s{2,}/g,
+            killBreaksRe:   /(<br[^\/>]*\/?>(\s|&nbsp;?)*){1,}/g,
+            linkRe1:        /<a[^>]*>/gi,
+            linkRe2:        /<\/a[^>]*>/gi,
+            imgRe:          /<img[^>]*>/gi,
+            imgSrc:         /src="([^"]*)/i,
+            pTag1:          /<p[^>]*>/gi,
+            pTag2:          /<\/p[^>]*>/gi
+        };
+
+    /*
+     * @method poorMansReadability
+     * @param {string} html
+     * @retrun {object}
+     */
+
+    function poorMansReadability(html) {
+
+        var data = {},
+            text,
+            images = [],
+            i,
+            src;
+
+        if (!html) {
+            return data;
+        }
+
+        images = html.match(regexps.imgRe);
+
+        text = html.replace(regexps.replaceBrsRe, '')
+            .replace(regexps.replaceFontsRe, '')
+            .replace(regexps.pTag1, '')
+            .replace(regexps.pTag2, '')
+            .replace(regexps.linkRe1, '')
+            .replace(regexps.linkRe2, '')
+            .replace(regexps.trimRe, '')
+            .replace(regexps.normalizeRe, '')
+            .replace(regexps.killBreaksRe, '')
+            .replace(regexps.imgRe, '');
+
+        data.body = text;
+        data.images = [];
+
+        for (i in images) {
+            if (images.hasOwnProperty(i)) {
+                src = images[i].match(regexps.imgSrc);
+
+                if (src && src[1]) {
+                    data.images.push({
+                        src: src[1]
+                    });
+                }
+            }
+        }
+
+        return data;
+    }
+
+    /*
+     * @method getCacheSize
+     * @retrun {int}
+     */
+
+    function getCacheSize(id) {
+
+        if (!cache[id]) {
+            return 0;
+        }
+
+        return cache[id].length;
+    }
+
+    /*
+     * @method setCacheValue
+     * @param {string} key
+     * @param {string} match
+     * @param {object} value
+     */
+
+    function setCacheValue(id, key, match, value) {
+
+        if (!cache[id]) {
+            cache[id] = [];
+        }
+
+        Y.Array.each(cache[id], function (object, pos) {
+            if (object[key] === match) {
+                cache[id][pos] = value;
+            }
+        });
+
+        cache[id].push(value);
+    }
+
+    /*
+     * @method getCacheValue
+     * @param {string} key
+     * @param {string} match
+     * @retrun {object}
+     */
+
+    function getCacheValue(id, key, match) {
+
+        var ret = null;
+
+        if (!cache[id]) {
+            return ret;
+        }
+
+        Y.Array.each(cache[id], function (object) {
+            if (object[key] === match) {
+                ret = object;
+            }
+        });
+
+        return ret;
+    }
+
+    /*
+     * @method inCache
+     * @param {string} key
+     * @param {string} match
+     * @retrun {boolean}
+     */
+
+    function inCache(id, key, match) {
+        return getCacheValue(id, key, match) ? true : false;
+    }
+
+    /*
+     * @method fillCache
+     * @param {string} query
+     * @param {function} cb
+     */
+
+    function fillCache(query, cb) {
+
+        Y.YQL(query + ' limit 100', function (data) {
+
+            var items;
+
+            if (!data.query || !data.query.results || !data.query.results.item) {
+                cb();
+                return;
+            }
+
+            items = data.query.results.item;
+
+            // Walk over the items and cache and add any we don't have
+            Y.Array.each(items, function (item) {
+
+                var read;
+
+                if (inCache(query, 'url', item.link) === false) {
+
+                    read = poorMansReadability(item.description);
+
+                    setCacheValue(query, 'url', item.link, {
+                        title: item.title,
+                        body: read.body,
+                        images: read.images,
+                        url: item.link,
+                        date: item.pubDate
+                    });
+                }
+            });
+
+            // Sort the cache by "date"
+            cache[query].sort(function (a, b) {
+                if (a.date > b.date) {
+                    return -1;
+                }
+                if (a.date < b.date) {
+                    return 1;
+                }
+                return 0;
+            });
+
+            cb();
+        });
+    }
+
+    /*
+     * @method getCacheSlice
+     * @param {string} query
+     * @param {int} limit
+     * @param {int} offset
+     * @param {function} cb
+     */
+
+    function getCacheSlice(query, limit, offset, cb) {
+
+        if (getCacheSize(query) < limit + offset) {
+            fillCache(query, function () {
+                cb(cache[query].slice(offset, offset + limit - 1));
+            });
+        } else {
+            cb(cache[query].slice(offset, offset + limit - 1));
+        }
+    }
+
+    Y.mojito.models.newsfeed = {
+
+        init: function (config) {
+            this.cfg = config;
+        },
+
+        getFeed: function (feedId, offset, limit, callback) {
+
+            // Convert the "feedId" in a YQL statment
+            var query = Y.Object.getValue(this.cfg, ['feeds', feedId, 'query']);
+
+            // If "query" has no value return an error
+            if (typeof query === 'undefined') {
+                callback('No feed id given.');
+                return;
+            }
+
+            // If "offset" is not a number or lower than zero fix it
+            if (!offset || offset < 0) {
+                offset = 0;
+            }
+
+            // If "limit" is not a number fix it
+            if (!limit) {
+                limit = this.cfg.defaultPageSize;
+            }
+
+            getCacheSlice(query, limit, offset, function (feed) {
+                callback(null, feed);
+            });
+        }
+    };
+
+}, '0.0.1', {
+    requires: ['yql']
+});
--- readme.md
+++ readme.md
@@ -20,3 +20,24 @@ Now go to [http://localhost:8666/](http://localhost:8666/)
 
 You can view the URL from a desktop web browser, an iPad and an iPhone or Android phone.
 Each one will provide you with a slightly different experience that fits the device.
+
+## Building Web Applications
+
+To build webapps for both "phone" and "tablet" run the following script.
+_Note:_ This command depends on an as yet un-applied Mojito patch [https://github.com/yahoo/mojito/pull/216](https://github.com/yahoo/mojito/pull/216).
+
+> ./scripts/webapp-build
+
+This creates two directories "./builds/webapp/phone" and "./builds/webapp/tablet".
+Each directory contains all the files required to run the application 100% in a browser, no server side processing is required.
+
+You can test the applications by running either;
+
+> ./scripts/webapp-run/phone.js
+
+or;
+
+> ./scripts/webapp-run/tablet.js
+
+And then going to [http://localhost:3000/index.html](http://localhost:3000/index.html) in a browser.
+_Note:_ Depending on the browser you are using you may have to force a reload of the URL to clear the cache.
