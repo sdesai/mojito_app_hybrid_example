ad498dc6feee0e1396b0ff2aac9479670045d8cd
Ric Allinson <capecodehq@yahoo.co.uk>
2012-08-07 13:38:17 -0700

    Added Android phonegap build files.

---
+++ builds/phonegap/android/phone/assets/www/static_phonegap_files/yahoo.hybrid.app_nav/binders/loader.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2012 Yahoo! Inc. All rights reserved.
+ */
+
+/*jslint nomen: true */
+
+/*global YUI: true, window: true, document: true*/
+
+'use strict';
+
+YUI.add('newsfeedappbinderloader', function (Y, NAME) {
+
+    Y.namespace('mojito.binders')[NAME] = {
+
+        init: function (mp) {
+            this.mp = mp;
+        },
+
+        bind: function (node) {
+
+            this.mp.invoke('index', {}, function (err, html) {
+
+                if (err) {
+                    console.log(err || html || 'No data given.');
+                    alert('An error has occurred. Please reload the page and try again.');
+                    return;
+                }
+
+                node.replace(html);
+            });
+        }
+    };
+
+}, '0.0.1', {
+    requires: ['mojito-client']
+});
